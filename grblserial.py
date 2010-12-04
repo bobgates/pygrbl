d5609be087c07eea96dc724338e18f38b6d7f516
bobgates <dvogt@polka.co.za>
2010-12-04 16:44:25 +0200

    Rewritten as an grblSerial as an object, and added a timer
    grblSerial is now a free-standing object that streams commands to grbl. It has a timer that updates its status, and can easily be integrated into a main program. 
    
    Still needs:
    - signals to indicate change of status
    - direct commands like 'emergency stop'
    
    Signed-off-by: bobgates

--- grblserial.py
+++ grblserial.py
@@ -7,25 +7,10 @@ naming scheme, but should be easy enough to change.
 import serial
 import time
 import sys
-
-class grbl_serial():
-    def __init__(self):
-        self.commandQueue = []
-        self.line_number=-1
-       
-      
-    def push(command):
-        self.commandQueue.push(command)
-        
+from PyQt4.QtCore import * #QTimer, QObject
 
 
-def send(block):
-    for i,c in enumerate(block):
-        if not ser.write(c):
-            raise Exception('Failed to write byte in string: %s' % block)
-        time.sleep(0.001)
-    
-class grblStatusT():
+class grblStatusT:
     x = 0.0;
     y = 0.0;
     z = 0.0;
@@ -33,87 +18,188 @@ class grblStatusT():
     bufferReady = False;
     machineRunning = False;
     autoMode = False;
+
+
+class grblSerial:
+    ''' Class that provides queue acccess to grbl
     
-def parseStatus(line):
-    '''Takes a line response from grbl to an 'eq' command,
-    which is in the form: ORN0X0Y0Z0, and fills a grblStatusT
-    class with the contents'''
+    Is initialized with a default serial port, but
+    if that doesn't work out, it can be called with
+    openSerial(portname) and a different port name.
     
-    result = grblStatusT()
+    Commands are sent to grbl on a timer tick, generated
+    by a QTimer, set to a 20ms period by default. Also at
+    each timer tick the status is updated.
     
-    result.mode = line[0]
-    result.bufferReady = (line[1]=='R')
-    result.machineRunning = not (line[0]=='O')
-    if result.machineRunning:
-        result.autoMode = (line[0]=='A')
-    else:
-        result.autoMode = False;
-    startn = line.find('N')
-    startx = line.find('X')
-    starty = line.find('Y')
-    startz = line.find('Z')
+    grblSerial maintains a command fifo queue, limited only
+    by available memory. It then attempts on each tick to run
+    recent commands out to grbl, if grbl is accepting commands.
     
-    if (startn<0) or (startx<0) or (starty<0) or (startz<0):
-        raise Exception('Bad format from eq line')
-    result.lineNumber = int(line[startn+1:startx])
-    result.x = float(line[startx+1:starty])
-    result.y = float(line[starty+1:startz])
-    result.z = float(line[startz+1:])
+    Immediate commands are sent outside of the command loop.
+    '''
     
-    return result    
-   
-def stripWhitespace(str):
-    line = str.replace(' ','')
-    line = line.replace('\t','')
-    line = line.replace('\r','')
-    line = line.replace('\n','')
-    return line
-   
-def getStatus():    
-    send('eq\n')
-    line = ser.readline()
-    if not ('EQ' in line):
-        raise Exception("EQ command failed")
-    line = ser.readline()
-    return parseStatus(line)
+
+    # Unsupported g-codes are simply not sent to grbl.
+    unsupported = ['G04', 'G43']  
+
+    def __init__(self):
+        '''Initialize grblSerial object with default 
+           serial port ID and set up object variables.
+        '''
+        self.portname = '/dev/tty.usbserial-A700dXL8'
+        self.ser = self.openSerial(self.portname)
+        self.commandQueue = []
+        self.line_number=-1
+        self.status = grblStatusT
+        self.timerInterval = 20		# milliseconds
+        self.timer = QTimer() 
+        QObject.connect(self.timer, SIGNAL("timeout()"), self.tick) 
+        self.timer.start(self.timerInterval);
         
-def sendCommand(command):
-    command = stripWhitespace(command).upper()
-    send(command+'\n')
+    def openSerial(self, portname):
+        '''Open serial port. This should reset grbl,
+        which is checked for. If a grbl isn't found
+        (or at least something that comes back with 
+        'ok', then raise an exception
+        '''
+        ser = serial.Serial(portname,115200, timeout=6)
+        
+        startedOk= False
+        for i in range(5):
+            line = ser.readline()
+            #print str(i)+': '+str(line)
+            if 'ok' in line:
+                startedOk = True
+                break
        
-    line = ser.readline()
-    line = stripWhitespace(line).upper()
+        if not startedOk:
+            raise Exception('Unable to connect to grbl')
+        
+        return ser
+    
+    def send(self, line):
+        '''Sends out the characters in a line with slight 
+        delays between them, required by grbl
+        '''
+    
+        for i,c in enumerate(line):
+            if not self.ser.write(c):
+                raise Exception('Failed to write byte in string: %s' % block)
+            time.sleep(0.001)
+            
+    def updateStatus(self):
+        '''Send a status query to grbl and update the
+        status variable.
+        '''    
+        self.send('eq\n')
+        line = self.ser.readline()
+        if not ('EQ' in line):
+            raise Exception("EQ command failed")
+        line = self.ser.readline()
+        self.status = self.parseStatus(line)
+
+    def parseStatus(self, line):
+        '''Takes a line response from grbl to an 'eq' command,
+        which is in the form: ORN0X0Y0Z0, and fills a grblStatusT
+        class with the contents'''
+        self.status.mode = line[0]
+        self.status.bufferReady = (line[1]=='R')
+        self.status.machineRunning = not (line[0]=='O')
+        if self.status.machineRunning:
+            self.status.autoMode = (line[0]=='A')
+        else:
+            self.status.autoMode = False;
+        startn = line.find('N')
+        startx = line.find('X')
+        starty = line.find('Y')
+        startz = line.find('Z')
+        
+        if (startn<0) or (startx<0) or (starty<0) or (startz<0):
+            raise Exception('Bad format from eq line')
+        self.status.lineNumber = int(line[startn+1:startx])
+        self.status.x = float(line[startx+1:starty])
+        self.status.y = float(line[starty+1:startz])
+        self.status.z = float(line[startz+1:])
+        return self.status    
        
-    if line != command:
-        return 0
-        raise Exception('Response:_'+line+'_ does not equal command:_'+command+'_')
+       
+    def getStatus(self):
+        '''Use this call to get the status, because
+        it updates the status from grbl before returning.
+        '''
+        self.updateStatus()
+        return self.status
+       
+    def stripWhitespace(self, str):
+        line = str.replace(' ','')
+        line = line.replace('\t','')
+        line = line.replace('\r','')
+        line = line.replace('\n','')
+        return line
+            
+    def sendCommand(self, command):
+        command = self.stripWhitespace(command).upper()
+        self.send(command+'\n')
+           
+        line = self.ser.readline()
+        line = self.stripWhitespace(line).upper()
+           
+        if line != command:
+            return 0
+            raise Exception('Response:_'+line+'_ does not equal command:_'+command+'_')
+            
+        line = self.ser.readline()
+        if not ('ok' in line):
+            return 0
+            raise Exception('Response to command:_'+command+'_ not okay: '+line)
+        return 1
         
-    line = ser.readline()
-    if not ('ok' in line):
-        return 0
-        raise Exception('Response to command:_'+command+'_ not okay: '+line)
-    return 1
         
-def openSerial(portname):
-    ser = serial.Serial(portname,115200, timeout=3)
-    
-    startedOk= False
-    for i in range(5):
-        line = ser.readline()
-        print str(i)+': '+str(line)
-        if 'ok' in line:
-            startedOk = True
-            break
-   
-    if not startedOk:
-        raise Exception('Unable to connect to grbl')
-    
-    return ser
+    def tick(self):
+        '''Timer update: checks if anything is in the
+        command queue, and adds it to grbl, until
+        either the queue is empty or grbl isn't accepting
+        any further commands because its buffer is full.
+        '''
+        self.updateStatus()
+        if len(self.commandQueue)>0:
+            while self.status.bufferReady:
+                command = self.commandQueue.pop(0)
+                #print command
+                
+                runCommand = True
+                #print 'Command:',command,'_'
+                if len(self.stripWhitespace(command))==0:
+                   runCommand = False;
+                for text in grblSerial.unsupported:
+                    if command.find(text)!=-1:
+                       runCommand = False
+                       break
+                #print runCommand      
+                if runCommand:
+                    print '\nSending: ',command
+                    if  not self.sendCommand(command):
+                        self.commandQueue.insert(0, command)
+                self.updateStatus()
+                        
+                        
+    def bufferEmpty(self):
+        return len(self.commandQueue)==0
+        
+    def addCommand(self, command):
+        self.commandQueue.append(command)
+
+    def close(self):
+        self.ser.close()
+
         
 if __name__ == '__main__':
-    ser = openSerial('/dev/tty.usbserial-A700dXL8')
-    
-        
+
+    '''Test program that creates a grblSerial object and
+    sends it a number of commands, either a short default
+    set or a longer set from a file.
+    '''
+            
     commands = ['F60N120G1x3y0z0', 
                 'F30N130g1x3y3z0',
                 'N140x0y3',
@@ -126,57 +212,38 @@ if __name__ == '__main__':
                 'n200g0z1',
                 'n210x0y0',
                 ]
-        
-        
-#    for command in commands:
-#        sendCommand(command)
 
-  
-    #file = open('cncweb.txt')
-    file = open('geebeepath.txt')
-    commands = file.readlines()
-    file.close()
-    #print commands  
-      
-    unsupported = ['G04', 'G43']  
-      
-            
-    oldLineNumber = 0
-    while True:
+#     file = open('cncweb.txt')
+#     #file = open('geebeepath.txt')
+#     commands = file.readlines()
+#     file.close()
+
+
+    grbl = grblSerial()
+
+    for command in commands:
+#        print 'adding'
+        grbl.addCommand(command)
+
+    while not grbl.bufferEmpty():
         time.sleep(0.1)
-        result = getStatus()
-        if len(commands)>0:
-            while result.bufferReady:
-                command = commands.pop(0)
-                #print command
-                
-                runCommand = True
-                #print 'Command:',command,'_'
-                if len(stripWhitespace(command))==0:
-                   runCommand = False;
-                for text in unsupported:
-                    if command.find(text)!=-1:
-                       runCommand = False
-                       break
-                #print runCommand      
-                if runCommand:
-                    print '\nSending: ',command
-                    if  not sendCommand(command):
-                        commands.insert(0, command)
-                result = getStatus()
-                        
-        if oldLineNumber != result.lineNumber:
-            oldLineNumber = result.lineNumber
-            print
-        print '\r', result.lineNumber, result.x, result.y, result.z,
+        grbl.tick()
+        result = grbl.getStatus()
+        print result.lineNumber,': ', result.x, result.y, result.z
         sys.stdout.flush()
-        if (not result.machineRunning) and (oldLineNumber!=0):
-            break    
     
-    
-    #time.sleep(10)
-    
-    ser.close()
+    #Run program for ten more seconds after grbl's buffer goes
+    #empty, because when the serial link is closed grbl resets, and
+    # its display gets zeroed. This allows me to at least check what
+    # it was displaying when the program ended.
+    for i in range(100):
+        time.sleep(0.1)
+        #grbl.tick()
+        result = grbl.getStatus()
+        print result.lineNumber,': ', result.x, result.y, result.z
+        sys.stdout.flush()
+
+    grbl.close()
 
 
 
